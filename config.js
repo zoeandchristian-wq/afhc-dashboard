485d432b4323c938a3d4be927e817a3d85186624
zoeandchristian-wq <zoeandchristian@americafirsthealthcare.com>
2026-06-01 15:41:25 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.AFHC_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzcxV3EL7Zdurx-Srk-AjgBYNdG1USpPDxZYq4-9FisBOaPsRdHEUoDxtU9CDzgeGSo/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyBTLMey5FU_OtIoiCbWtzMN-ex5JwJnKnSs7P-lr6xBfIBFE2z6h5SukCGx-wLUVRD/exec",
   token: "MIXzGjak8TDbJO0Tn6ipJPOa6uh1j5U9"
 };
