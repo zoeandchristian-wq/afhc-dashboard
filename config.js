0a76302b3278ed820dfff99d9af79cf570347da6
zoeandchristian-wq <zoeandchristian@americafirsthealthcare.com>
2026-06-01 14:11:32 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.AFHC_CONFIG = {
+  apiUrl: "https://script.google.com/macros/s/AKfycbzcxV3EL7Zdurx-Srk-AjgBYNdG1USpPDxZYq4-9FisBOaPsRdHEUoDxtU9CDzgeGSo/exec",
+  token: "MIXzGjak8TDbJO0Tn6ipJPOa6uh1j5U9"
+};
